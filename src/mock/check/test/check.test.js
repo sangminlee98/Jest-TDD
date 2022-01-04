4303f47a24cceb6dbb4a2671af17d948150cbf00
sangminlee98 <tkdals0975@naver.com>
2022-01-04 18:49:27 +0900

    use mock fn with no dependecny injection

--- src/mock/check/test/check.test.js
+++ src/mock/check/test/check.test.js
@@ -2,30 +2,19 @@ const check = require('../check.js');
 
 describe('check', () => {
     let onSuccess;
-    let onFail
-
+    let onFail;
     beforeEach(() => {
         onSuccess = jest.fn();
         onFail = jest.fn();
     });
-
     it('should call onSuccess when predicate is true', () => {
-        check(() => true, onSuccess, onFail);
-        // expect(onSuccess.mock.calls.length).toBe(1);
+        check(()=>true, onSuccess, onFail);
         expect(onSuccess).toHaveBeenCalledTimes(1);
-        // expect(onSuccess.mock.calls[0][0]).toBe('yes');
-        expect(onSuccess).toBeCalledWith('yes');
-        // expect(onFail.mock.calls.length).toBe(0);
-        expect(onFail).toBeCalledTimes(0);
+        expect(onSuccess).toHaveBeenCalledWith('yes');
     });
-
     it('should call onFail when predicate is false', () => {
-        check(() => false, onSuccess, onFail);
-        // expect(onFail.mock.calls.length).toBe(1);
+        check(()=>false, onSuccess, onFail);
         expect(onFail).toHaveBeenCalledTimes(1);
-        // expect(onFail.mock.calls[0][0]).toBe('no');
         expect(onFail).toHaveBeenCalledWith('no');
-        // expect(onSuccess.mock.calls.length).toBe(0);
-        expect(onSuccess).toHaveBeenCalledTimes(0);
     })
 });
---
+++ src/mock/product/test/product_service_no_di.test.js
@@ -0,0 +1,26 @@
+const ProductService = require('../product_service_no_di');
+const ProductClient = require('../product_client');
+jest.mock('../product_client');
+
+
+describe('ProductService', () => {
+    const fetchItems = jest.fn(async () => [
+        {item: 'Milk', available: true},
+        {item: 'Banana', available: false},
+    ]);
+    ProductClient.mockImplementation(() => {
+        return {
+            fetchItems,
+        }
+    })
+    let productService;
+    beforeEach(() => {
+        productService = new ProductService();
+    });
+
+    it('should filter out only available items', async () => {
+        const items = await productService.fetchAvailableItems();
+        expect(items).toEqual([{item: 'Milk', available: true}]);
+        expect(items).toHaveLength(1);
+    })
+})
